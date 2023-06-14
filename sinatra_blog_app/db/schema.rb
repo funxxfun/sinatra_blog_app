5e3df3ae69c6c3d25df843026e86946265e6f10e
funxxfun <23shirasawa@gmail.com>
2023-06-14 14:20:49 +0900

    いいねの数を表示する

--- sinatra_blog_app/db/schema.rb
+++ sinatra_blog_app/db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_11_062548) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_14_043430) do
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
     t.text "title"
